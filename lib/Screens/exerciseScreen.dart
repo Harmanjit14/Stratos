f49ec5d8e947b7c3fbb59f014caf5e292ad908f8
Harmanjit Singh <harmanjit140500@gmail.com>
2021-02-28 01:30:52 +0530

    camera

--- lib/Screens/exerciseScreen.dart
+++ lib/Screens/exerciseScreen.dart
@@ -1,12 +1,12 @@
 import 'package:Stratos/main.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class ExerciseScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      
+      child: Container(),
     );
   }
 }
-
